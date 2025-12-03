708a6224a83e762287ce6f1bdceeb609ebc805e7
setayesh-kamali <stud106727@fh-wedel.de>
2025-12-03 19:08:13 +0100

    Add initial ecommerce package and classes

---
+++ oop-basics/ecommerce/Product.java
@@ -0,0 +1,41 @@
+package ecommerce;
+
+public class Product {
+
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+
+    public Product(int id, String name, double price, int stock) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void decreaseStock(int amount) {
+        this.stock -= amount;
+    }
+
+    @Override
+    public String toString() {
+        return id + ") " + name + " - " + price + " € (Stock: " + stock + ")";
+    }
+}
